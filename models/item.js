34ef984a4b19d0d25a80687603c9ea05d4048348
Hesham243 <heshamahmed243@gmail.com>
2025-08-10 23:31:11 +0300

    created store.js and item.js  models with thier appropriat properties and data types

---
+++ models/item.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+
+const reviewSchema = new mongoose.Schema({
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  text: { 
+    type: String, 
+    required: true 
+  },
+  rating: { 
+    tupe: Number, 
+    enum: [1, 2, 3, 4, 5], 
+    required: true, 
+  },
+}, { timestamps: true })
+
+
+const itemSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true, 
+  },
+  description: { 
+    type: String, 
+  },
+  price: { 
+    type: Number, 
+    required: true, 
+  },
+  image: { 
+    type: String 
+  },
+  category: { 
+    type: String, 
+    enum: ['smartphones', 'laptops', 'clothing', 'furniture', 'kitchen', 'stationery', 'fitness'], 
+    required: true, 
+  },
+
+  review: [reviewSchema],
+
+}, { timestamps: true })
+
+
+
+const Item = mongoose.model('Item', itemSchema)
+module.exports = Item
---
+++ models/store.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+
+const storeSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true 
+  },
+  logo: { 
+    type: String 
+  },
+  category: { 
+    type: String, 
+    enum: ['electronics', 'toys', 'books', 'sports', 'fashion', 'health_beauty'], 
+    required: true 
+  },
+
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }],
+
+}, { timestamps: true })
+
+
+
+const Store = mongoose.model('Store', storeSchema)
+module.exports = Store
